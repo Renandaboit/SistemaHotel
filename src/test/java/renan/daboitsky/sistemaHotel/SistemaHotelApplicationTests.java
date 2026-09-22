c75cc4f61ac0bc610c7057697fde98f75eaedfac
Renan Daboit <renan_daboit@estudante.sesisenai.org.br>
2026-09-22 20:59:22 -0300

    primeiro commit

---
+++ src/test/java/renan/daboitsky/sistemaHotel/SistemaHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package renan.daboitsky.sistemaHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
